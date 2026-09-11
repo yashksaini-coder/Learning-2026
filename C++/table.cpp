43e542797fc5069f2c50e4555856f5e10f56ca74
yashksaini-coder <yashksaini89@gmail.com>
2026-09-11 14:35:02 +0530

    Table of N

---
+++ C++/table.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int n;
+  cin >> n;
+
+  for (int i = 1; i <= 10; i++) {
+    cout << n << "*" << i << "=" << n * i << endl;
+  }
+  return 0;
+}
