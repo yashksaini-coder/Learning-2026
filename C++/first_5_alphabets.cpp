cdeaa98135499f5f9ee00e1c7710d08938469290
yashksaini-coder <yashksaini89@gmail.com>
2026-09-11 14:29:11 +0530

    Add first 5 alphabets

---
+++ C++/first_5_alphabets.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "A" << endl;
+  cout << "B" << endl;
+  cout << "C" << endl;
+  cout << "D" << endl;
+  cout << "E" << endl;
+  return 0;
+}
