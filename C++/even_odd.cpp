f4a05b31a93e22a549bbc941d04142725d1f1d85
yashksaini-coder <yashksaini89@gmail.com>
2026-09-12 12:01:15 +0530

    even_odd

---
+++ C++/even_odd.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int x;
+  cin >> x;
+
+  if (x % 2 == 0)
+    cout << "Even" << endl;
+  cout << "Odd" << endl;
+
+  return 0;
+}
