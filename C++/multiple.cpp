84636d2224f59bde77dea16c940de83333ddde98
yashksaini-coder <yashksaini89@gmail.com>
2026-09-12 12:10:58 +0530

    Multiple program

---
+++ C++/multiple.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int x, y;
+  cin >> x >> y;
+  if (x / y == 0)
+    cout << "Yes";
+  else
+    cout << "No";
+  return 0;
+}
