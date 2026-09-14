ff36c3fd3d87c772fbc6825f8c17f498c6058ef8
yashksaini-coder <yashksaini89@gmail.com>
2026-09-14 21:34:10 +0530

    While loop print 1 to 10

---
+++ Loops/1_to_10.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int i = 1;
+  while (i <= 10) {
+    cout << i << endl;
+    i++;
+  }
+
+  return 0;
+}
