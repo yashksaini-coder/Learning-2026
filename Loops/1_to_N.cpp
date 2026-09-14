9868ee6ac42e2554b7871210088e8a104cb6ccd7
yashksaini-coder <yashksaini89@gmail.com>
2026-09-14 22:08:31 +0530

    Print 1 to N while loop

---
+++ Loops/1_to_N.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, i = 1;
+  cin >> n;
+
+  while (i <= n) {
+    cout << i << endl;
+    i++;
+  }
+
+  return 0;
+}
