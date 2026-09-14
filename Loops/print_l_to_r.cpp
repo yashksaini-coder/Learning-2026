a29c547e20e58fe0085cdc6d9afa7c994b9d8156
yashksaini-coder <yashksaini89@gmail.com>
2026-09-14 22:19:31 +0530

    Print from L to R

---
+++ Loops/print_l_to_r.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int l, r;
+
+  cin >> l >> r;
+  int i = l;
+  while (i <= r) {
+    cout << i << endl;
+    i++;
+  }
+
+  return 0;
+}
